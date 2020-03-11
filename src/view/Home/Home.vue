f158e07e2b1f375c0869250516b9f672ba14a0dc
feng <254869354@qq.com>
2020-03-11 22:40:47 +0800

    eslint

---
+++ src/view/Home/Home.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        this is home page, open to all
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                obj: {id:1,name:'feng',val:"1"}
+            }
+        },
+        created() {
+
+        },
+        methods: {
+
+        }
+    }
+</script>
+
+<style scoped lang="less">
+
+</style>
